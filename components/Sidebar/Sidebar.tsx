2af11b161a271f6f78131b5b16bd502a44797634
Greg Zaal <gregzzmail@gmail.com>
2021-04-10 11:13:30 +0200

    Fix missing key, add "All" cat

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -7,9 +7,12 @@ import styles from './Sidebar.module.scss';
 const Sidebar = (props) => {
   return (
     <div id={styles.sidebar}>
+      <Link href={`/${props.assetType}`}>
+        <div key="All">All</div>
+      </Link>
       {tlc[props.assetType].map(tlc => (
-        <Link href={`/${props.assetType}/${tlc}`}>
-          <div key={tlc}>{tlc}</div>
+        <Link href={`/${props.assetType}/${tlc}`} key={tlc}>
+          <div>{tlc}</div>
         </Link>
       ))}
     </div>
