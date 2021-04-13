b6bdeab3ab41cbc6ac9b159105055ee973bb88ca
Greg Zaal <gregzzmail@gmail.com>
2021-04-13 16:19:48 +0200

    Dynamic sidebar stuff :) This shouldn't have been so hard...

---
+++ components/Sidebar/CategoryList.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link'
+import useSWR from 'swr';
+import fetcher from 'utils/fetcher';
+
+import tlc from 'constants/top_level_categories.json';
+
+import Spinner from 'components/Spinner/Spinner';
+
+import styles from './Sidebar.module.scss';
+
+const CategoryList = (props) => {
+  if (props.level > 2) {
+    return <></>
+  }
+  const activeCat = props.categories[props.level + 1];
+  let url = `https://api.polyhaven.com/categories/${props.assetType}`;
+  const in_cats = props.categories.slice(0, Math.max(0, props.level + 1))
+  if (in_cats.length) {
+    url += `?in=${in_cats.join(',')}`
+  }
+  const { data, error } = useSWR(url, fetcher, { revalidateOnFocus: false });
+
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  if (!data) {
+    return <Spinner />;
+  }
+
+  const wholeList = props.level === -1 ? tlc[props.assetType] : Object.keys(data);
+  const list = wholeList.filter(cat => (
+    data[cat] > 2
+  ))
+
+  const level = props.level + 1;
+
+  return (
+    <div className={`${level > 0 ? styles.subCat : ""}`}>
+      {list.map(cat => (
+        <div key={cat}>
+          <Link href={`/${props.assetType}/${in_cats.length ? in_cats.join('/') + '/' : ''}${cat}`} >
+            <div className={` ${styles.cat} ${cat === activeCat ? styles.catActive : ""}`}>{cat}
+              <div className={styles.num}>
+                {data[cat] ? data[cat] : 0}
+              </div>
+            </div>
+          </Link>
+          {cat === activeCat ?
+            <CategoryList
+              assetType={props.assetType}
+              categories={props.categories}
+              level={level} />
+            : ""}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryList
+
+
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,39 +1,21 @@
 import Link from 'next/link'
-import useSWR from 'swr';
-import fetcher from 'utils/fetcher';
 
 import tlc from 'constants/top_level_categories.json';
 
-import Spinner from 'components/Spinner/Spinner';
+import CategoryList from './CategoryList';
 
 import styles from './Sidebar.module.scss';
 
 const Sidebar = (props) => {
-  const url = `https://api.polyhaven.com/categories/${props.assetType}`
-  const { data, error } = useSWR(url, fetcher, { revalidateOnFocus: false });
-
-  if (error) {
-    return <div id={styles.sidebar}>Error</div>;
-  }
-
-  if (!data) {
-    return <div id={styles.sidebar}><Spinner /></div>;
-  }
-
   return (
     <div id={styles.sidebar}>
       <Link href={`/${props.assetType}`}>
-        <div className={styles.tlc} key="all">All</div>
+        <div className={styles.cat} key="all">All</div>
       </Link>
-      {tlc[props.assetType].map(tlc => (
-        <Link href={`/${props.assetType}/${tlc}`} key={tlc}>
-          <div className={`${styles.tlc} ${tlc === props.categories[0] ? styles.tlcActive : ""}`}>{tlc}
-            <div className={styles.num} id={`tlc_${tlc}`}>
-              {data[tlc] ? data[tlc] : 0}
-            </div>
-          </div>
-        </Link>
-      ))}
+      <CategoryList
+        assetType={props.assetType}
+        categories={props.categories}
+        level={-1} />
     </div>
   );
 }
