74e63f8a59b00a6969705e02414ae824b06b0ec8
Greg Zaal <gregzzmail@gmail.com>
2021-04-20 18:27:53 +0200

    Switch to Open Sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default class CustomDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet" />
           <meta
             name="description"
             content="The Public Asset Library"
