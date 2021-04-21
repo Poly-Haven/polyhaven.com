c766e3ef4bcfc85eea830eeedfeab54027f12df3
Greg Zaal <gregzzmail@gmail.com>
2021-04-21 14:58:55 +0200

    Work on AssetPage

--- components/AssetPage/AssetPage.tsx
+++ components/AssetPage/AssetPage.tsx
@@ -1,8 +1,65 @@
+import styles from './AssetPage.module.scss'
+import Footer from 'components/Footer/Footer'
+
 const AssetPage = ({ assetID, data }) => {
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>by {Object.keys(data.authors).join(',')}</p>
+    <div className={styles.wrapper}>
+      <div className={styles.pageWrapper}>
+        <div className={styles.page}>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+          <h1>{data.name}</h1>
+          <p>by {Object.keys(data.authors).join(',')}</p>
+        </div>
+        <Footer />
+      </div>
+      <div className={styles.sidebar}>
+        Download
+      </div>
     </div>
   )
 }
