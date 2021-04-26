826e1d6f071d29bfb8ed6815563b57e5716b228e
Greg Zaal <gregzzmail@gmail.com>
2021-04-26 11:07:57 +0200

    Logo link & tweaks

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import Nav from './Nav/Nav'
 
 import styles from './Header.module.scss';
@@ -5,12 +7,12 @@ import styles from './Header.module.scss';
 const header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>
+      <Link href='/'><a className={styles.logo}>
         <div className={styles.logo_image}>
           <img src='/Logo 256.png' />
         </div>
         Poly Haven
-      </div>
+      </a></Link>
       <div className={styles.spacer} />
       <Nav />
     </div>
