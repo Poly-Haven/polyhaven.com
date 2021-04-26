18f8c6c524fd4299eb66e565dc87d4f52943a63c
Greg Zaal <gregzzmail@gmail.com>
2021-04-26 18:43:32 +0200

    Footer content

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,4 +1,13 @@
+import Link from 'next/link';
+
 import { randomArraySelection } from 'utils/arrayUtils'
+
+import { SiDiscord } from "react-icons/si";
+import { SiPatreon } from "react-icons/si";
+import { SiFacebook } from "react-icons/si";
+import { SiTwitter } from "react-icons/si";
+
+import Todo from 'components/Todo/Todo'
 
 import styles from './Footer.module.scss';
 
@@ -33,6 +42,45 @@ const footer = () => {
       <h2>Patrons</h2>
       <div className={styles.patrons}>
         {names_list.map((n, i) => <p key={i} className={styles[`patron-rank-${randomArraySelection(pledge_rank_weights)}`]}>{n}</p>)}
+      </div>
+      <Todo>TODO Corporate Sponsor Logos</Todo>
+      <div className={styles.linksWrapper}>
+        <div className={styles.links}>
+          <Link href="/"><a>
+            <div className={styles.logoWrapper}>
+              <img src='/Logo 256.png' className={styles.logo} />
+              <h1>Poly Haven</h1>
+              <p>The Public Asset Library</p>
+            </div>
+          </a></Link>
+          <div className={styles.linkListWrapper}>
+            <div className={styles.linkList}>
+              <Link href="/about-contact"><a>About</a></Link>
+              <Link href="/about-contact"><a>Contact</a></Link>
+              <Link href="https://bit.ly/ph-advertising"><a>Advertising</a></Link>
+            </div>
+          </div>
+          <div className={styles.linkListWrapper}>
+            <div className={styles.linkList}>
+              <Link href="/license"><a>License</a></Link>
+              <Link href="/privacy"><a>Privacy</a></Link>
+              <Link href="/finance-reports"><a>Finance Reports</a></Link>
+            </div>
+          </div>
+          <div className={styles.linkListWrapper}>
+            <div className={styles.linkList}>
+              <Link href="/faq"><a>FAQ</a></Link>
+              <Link href="/contribute"><a>Contribute</a></Link>
+              <Link href="https://blog.hdrihaven.com"><a>Blog</a></Link>
+            </div>
+          </div>
+          <div className={styles.communityIcons}>
+            <a href="https://discord.gg/Dms7Mrs"><SiDiscord /></a>
+            <a href="https://polyhaven.com/support-us"><SiPatreon /></a>
+            <a href="https://polyhaven.com/facebook"><SiFacebook /></a>
+            <a href="https://polyhaven.com/twitter"><SiTwitter /></a>
+          </div>
+        </div>
       </div>
     </div>
   );
