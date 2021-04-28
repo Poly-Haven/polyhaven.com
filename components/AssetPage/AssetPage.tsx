c3149ea9dc4297508d9801fcb71fd319a539838c
Greg Zaal <gregzzmail@gmail.com>
2021-04-28 19:45:02 +0200

    WIP image previews on asset pages

--- components/AssetPage/AssetPage.tsx
+++ components/AssetPage/AssetPage.tsx
@@ -8,17 +8,28 @@ import AuthorCredit from 'components/AuthorCredit/AuthorCredit'
 import styles from './AssetPage.module.scss'
 
 const AssetPage = ({ assetID, data }) => {
+  const ext = {
+    0: 'jpg',
+    1: 'png',
+    2: 'png'
+  }
   const authors = Object.keys(data.authors)
   const multiAuthor = authors.length > 1;
   return (
     <div className={styles.wrapper}>
-      <Page>
+      <Page immersiveScroll={true}>
         <div className={styles.header}>
           <h1>{data.name}</h1>
           <div className={styles.spacer} />
           <div className={styles.authors}>
             Author{multiAuthor ? "s" : ""}:
             {authors.map(a => <AuthorCredit id={a} key={a} credit={multiAuthor ? data.authors[a] : ""} />)}
+          </div>
+        </div>
+        <div className={styles.previewWrapper}>
+          <div className={styles.carousel}><Todo>Image carousel</Todo></div>
+          <div className={styles.activePreview}>
+            <img src={`https://cdn.polyhaven.com/asset_img/primary/${assetID}.${ext[data.type]}?width=1439&sharpen=true`} />
           </div>
         </div>
         <Todo />
