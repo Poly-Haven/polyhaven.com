09a7cd28e5a4dffafb1ae6e65853612e2c9d6604
Greg Zaal <gregzzmail@gmail.com>
2021-05-18 22:46:56 +0200

    Hide ads for now, will implement after launch.

--- components/Ads/AssetSidebar.tsx
+++ components/Ads/AssetSidebar.tsx
@@ -1,6 +1,7 @@
 import styles from './Ads.module.scss'
 
 const Ad = () => {
+  return (<></>)
   return (
     <img src="https://via.placeholder.com/336x280" className={styles.placeholder} />
   )
--- components/Ads/GridTop.tsx
+++ components/Ads/GridTop.tsx
@@ -1,6 +1,7 @@
 import styles from './Ads.module.scss'
 
 const Ad = () => {
+  return (<></>)
   return (
     <img src="https://via.placeholder.com/728x90" className={styles.placeholder} />
   )
