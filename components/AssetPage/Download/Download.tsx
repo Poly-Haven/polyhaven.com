19d04f3338497105c89307150cd040cc4430c83c
Greg Zaal <gregzzmail@gmail.com>
2021-06-02 19:56:26 +0200

    HDRIs are now downloadable

--- components/AssetPage/Download/Download.tsx
+++ components/AssetPage/Download/Download.tsx
@@ -24,6 +24,8 @@ const Download = ({ assetID, data }) => {
   const [prefRes, setRes] = useState('4k')
   const [prefFmt, setFmt] = useState('exr')
 
+  const isHDRI = data.type === 0
+
   useEffect(() => {
     setRes(localStorage.getItem(`assetPref_${data.type}_resolution`) || '4k')
     setFmt(localStorage.getItem(`assetPref_${data.type}_format`) || 'exr')
@@ -31,14 +33,14 @@ const Download = ({ assetID, data }) => {
 
   const { data: files, error } = apiSWR(`/files/${assetID}`, { revalidateOnFocus: false });
   if (error) {
-    return <div><div id="download-btn" className={styles.downloadBtn}>Error!</div></div>
+    return <div><div className={styles.downloadBtn}>Error!</div></div>
   } else if (!files) {
-    return <div><div id="download-btn" className={styles.downloadBtn}><Loader /></div></div>
+    return <div><div className={styles.downloadBtn}><Loader /></div></div>
   }
 
   // Find a key that we can use to fetch available resolutions and formats.
   let baseKey = 'UNKNOWN'
-  const baseKeys = ['HDRI', 'Diffuse', 'nor_gl', 'Rough', 'Displacement']
+  const baseKeys = ['hdri', 'Diffuse', 'nor_gl', 'Rough', 'Displacement']
   for (const k of baseKeys) {
     if (Object.keys(files).includes(k)) {
       baseKey = k
@@ -46,7 +48,7 @@ const Download = ({ assetID, data }) => {
     }
   }
   if (baseKey === 'UNKNOWN') {
-    return <div><div id="download-btn" className={styles.downloadBtn}>baseKey unknown!</div></div>
+    return <div><div className={styles.downloadBtn}>baseKey unknown!</div></div>
   }
 
   const toggleDlOptions = () => {
@@ -60,21 +62,19 @@ const Download = ({ assetID, data }) => {
   }
   const dlRes = resOptions.includes(prefRes) ? prefRes : resOptions.slice(-1)[0]
 
-  const fmtOptions = data.type === 0 ? ['hdr', 'exr'] : ['blend', 'gltf'];
+  const fmtOptions = isHDRI ? ['hdr', 'exr'] : ['blend', 'gltf'];
   const setFmtValue = v => {
     setFmt(v.value)
     localStorage.setItem(`assetPref_${data.type}_format`, v.value)
   }
-  const dlFmt = fmtOptions.includes(prefFmt) ? prefFmt : (data.type === 0 ? 'hdr' : 'blend')
+  const dlFmt = fmtOptions.includes(prefFmt) ? prefFmt : (isHDRI ? 'hdr' : 'blend')
 
   let fsize = 0
-  if (data.type !== 0) {
-    const fileInfo = files[dlFmt][dlRes][dlFmt]
-    fsize = fileInfo.size
-    if (fileInfo.include) {
-      for (const i of Object.values(fileInfo.include)) {
-        fsize += i['size']
-      }
+  const fileInfo = files[isHDRI ? 'hdri' : dlFmt][dlRes][dlFmt]
+  fsize = fileInfo.size
+  if (fileInfo.include) {
+    for (const i of Object.values(fileInfo.include)) {
+      fsize += i['size']
     }
   }
 
@@ -122,17 +122,23 @@ const Download = ({ assetID, data }) => {
           onChange={setFmtValue}
           small={true} />
 
-        <div id="download-btn" className={`${styles.downloadBtn} ${busyDownloading ? styles.disabled : null}`} onClick={downloadZip}>
+        <a
+          href={isHDRI ? files['hdri'][dlRes][dlFmt].url : null}
+          className={`${styles.downloadBtn} ${busyDownloading ? styles.disabled : null}`}
+          onClick={isHDRI ? null : downloadZip}
+        >
           <MdFileDownload />
           <div>
             <h3>Download</h3>
             <p>{filesize(fsize)}</p>
           </div>
           <div className={`${styles.busyDownloading} ${busyDownloading ? styles.show : null}`}><Loader /></div>
-        </div>
-        <div className={styles.downloadBtnSml} onClick={toggleDlOptions}>
-          {dlOptions ? <MdArrowBack /> : <MdMenu />}
-        </div>
+        </a>
+        {isHDRI ? null :
+          <div className={styles.downloadBtnSml} onClick={toggleDlOptions}>
+            {dlOptions ? <MdArrowBack /> : <MdMenu />}
+          </div>
+        }
       </div>
       <DownloadOptions open={dlOptions} files={files} res={dlRes} format={dlFmt} />
     </div>
