39306fb3f9eebbf3793510f585d3fef88445c683
Greg Zaal <gregzzmail@gmail.com>
2021-06-22 09:18:23 +0200

    Update privacy page:
    * We don't log IP addresses anymore, instead we log a UUID with consent.
    * Change header of "Browser Storage" section to "Download Preferences"
    * Add "The 30 newest patrons are also shown on the home page."
    * Remove "These names are publically accessibly on the Patreon website"

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -16,16 +16,17 @@ const PrivacyPage = () => {
       <p>Click here to read <a href="https://policies.google.com/technologies/partner-sites">Google's Privacy Policy</a>.</p>
       <p>If you are not comfortable with this, you can opt out of personalized advertising in your <a href="https://www.google.com/settings/ads">Ads Settings</a>, or use an ad-blocker such as <a href="https://github.com/gorhill/uBlock">uBlock Origin</a>.</p>
 
-      <h2>IP Logging</h2>
-      <p>When you download an asset, we log an anonymized version of your IP address, along with some information about the resolution and formats you downloaded. This helps us to see which assets are more popular and what people are generally looking for, so we can make decisions about what to create next.</p>
-      <p>If you're not comfortable with this, you can use a VPN to obscure your public IP address. If you wish for us to delete all recorded downloads from your IP address, you can contact us.</p>
+      <h2>UUID</h2>
+      <p>When you first visit this site and accept the Cookie popup, we store a unique random identifier in your browser, which is sent to us when you download an asset (along with some information about the resolution and formats you downloaded). This helps us to see which assets are more popular and what people are generally looking for, so we can make decisions about what to create next.</p>
+      <p>If you wish for us to delete all recorded downloads from your UUID, you can contact us.</p>
 
-      <h2>Browser Storage</h2>
+      <h2>Download Preferences</h2>
       <p>If you customize the resolution or format options when downloading an asset, this information is stored permanently in your browser so that it can be used again for subsequent downloads to save you time.</p>
 
       <h2>Patron Names in Footer</h2>
       <p>The user names of all active supporters on <a href="https://www.patreon.com/polyhaven/overview">Patreon</a> are automatically shown in the footer of every page on this site.</p>
-      <p>These names are publically accessibly on the Patreon website, but if you would like your name in the footer removed or changed, please contact me by direct message on Patreon.</p>
+      <p>The 30 newest patrons are also shown on the home page.</p>
+      <p>If you would like your name removed or changed, please contact me by direct message on Patreon.</p>
 
       <hr />
 
