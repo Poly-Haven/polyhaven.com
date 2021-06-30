e1949efab6849effaacb63f2b855d8844a08959e
Greg Zaal <gregzzmail@gmail.com>
2021-06-30 15:35:55 +0200

    Add instagram link

--- components/SocialIcons/SocialIcons.tsx
+++ components/SocialIcons/SocialIcons.tsx
@@ -2,6 +2,7 @@ import { SiDiscord } from "react-icons/si";
 import { SiPatreon } from "react-icons/si";
 import { SiFacebook } from "react-icons/si";
 import { SiTwitter } from "react-icons/si";
+import { SiInstagram } from "react-icons/si";
 import { SiYoutube } from "react-icons/si";
 
 import styles from './SocialIcons.module.scss'
@@ -13,6 +14,7 @@ const SocialIcons = () => {
       <a href="https://www.patreon.com/polyhaven/overview"><SiPatreon /></a>
       <a href="https://www.facebook.com/polyhaven"><SiFacebook /></a>
       <a href="https://twitter.com/polyhaven"><SiTwitter /></a>
+      <a href="https://www.instagram.com/polyhaven"><SiInstagram /></a>
       <a href="https://www.youtube.com/c/PolyHaven"><SiYoutube /></a>
     </div>
   )
