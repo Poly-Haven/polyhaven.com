7ec1b1ecd60a8ddefce61534119bffbb1ddb9b9a
Greg Zaal <gregzzmail@gmail.com>
2021-06-30 14:31:13 +0200

    Support `POLYHAVEN_API='LIVE'` env var to ensure use of the live API

--- utils/apiSWR.tsx
+++ utils/apiSWR.tsx
@@ -2,6 +2,6 @@ import useSWR from 'swr';
 import fetcher from 'utils/fetcher';
 
 export default function apiSWR(url: string, options) {
-  const baseUrl = process.env.NODE_ENV == "production" ? "https://api.polyhaven.com" : "http://localhost:3000"
+  const baseUrl = (process.env.NODE_ENV == "production" || process.env.POLYHAVEN_API == "live") ? "https://api.polyhaven.com" : "http://localhost:3000"
   return useSWR(`${baseUrl}${url}`, fetcher, options);
 }
