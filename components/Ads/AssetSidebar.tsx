3ff6dff226c10a3b237f2c98c6efcc159ce277e0
Greg Zaal <gregzzmail@gmail.com>
2021-07-04 17:19:56 +0200

    Insert ads. TODO: hide for patrons.

--- components/Ads/AssetSidebar.tsx
+++ components/Ads/AssetSidebar.tsx
@@ -1,10 +1,23 @@
 import styles from './Ads.module.scss'
 
 const Ad = () => {
-  return (<></>)
-  return (
-    <img src="https://via.placeholder.com/336x280" className={styles.placeholder} />
-  )
+  // TODO hide for patrons
+  if (process.env.NODE_ENV !== "production") {
+    return (
+      <img src="https://via.placeholder.com/336x280" className={styles.placeholder} />
+    )
+  }
+
+  return (<>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins className="adsbygoogle"
+      style={{ display: "inline-block", width: "336px", height: "280px" }}
+      data-ad-client="ca-pub-2284751191864068"
+      data-ad-slot="9249051205"></ins>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({ });
+    </script>
+  </>)
 }
 
 export default Ad
--- components/Ads/GridTop.tsx
+++ components/Ads/GridTop.tsx
@@ -1,10 +1,23 @@
 import styles from './Ads.module.scss'
 
 const Ad = () => {
-  return (<></>)
-  return (
-    <img src="https://via.placeholder.com/468x60" className={styles.placeholder} />
-  )
+  // TODO hide for patrons
+  if (process.env.NODE_ENV !== "production") {
+    return (
+      <img src="https://via.placeholder.com/468x60" className={styles.placeholder} />
+    )
+  }
+
+  return (<>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins className="adsbygoogle"
+      style={{ display: "inline-block", width: "468px", height: "60px" }}
+      data-ad-client="ca-pub-2284751191864068"
+      data-ad-slot="9488083725"></ins>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({ });
+    </script>
+  </>)
 }
 
 export default Ad
