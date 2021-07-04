132e5a6188b66f14366806d5b9271d1fecdcffc5
James Hamilton <james@thechipfactory.co.uk>
2021-07-04 11:42:03 +0900

    first working version WIP

--- components/AssetPage/WebGL/GLTFViewer.tsx
+++ components/AssetPage/WebGL/GLTFViewer.tsx
@@ -3,10 +3,28 @@ import apiSWR from 'utils/apiSWR'
 import Loader from 'components/UI/Loader/Loader'
 
 import styles from './GLTFViewer.module.scss'
+import { Suspense } from "react";
 
-const GLTFViewer = ({ show, assetID }) => {
+import * as THREE from 'three'
+import React, { FC, useRef, useState } from 'react'
+
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei'
+import { useEffect } from 'react';
+
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+interface Props {
+  readonly show: boolean;
+  readonly assetID: string;
+}
+
+const GLTFViewer: FC<Props> = ({ show, assetID }) => {
   if (!show) return null
 
+  const [loaded, setLoaded] = useState(false);
+  // const [GLTF, setGLTF] = useState(null);
   const { data: files, error } = apiSWR(`/files/${assetID}`, { revalidateOnFocus: false });
   if (error) {
     return <div className={styles.wrapper}>No files?<br /><em>Try refresh, otherwise please report this to us.</em></div>
@@ -21,13 +39,62 @@ const GLTFViewer = ({ show, assetID }) => {
     return <div className={styles.wrapper}>No preview available for this model, sorry :(</div>
   }
 
-  const gltfFiles = files['gltf']['4k']
+  // const gltfFiles = files['gltf']['4k']
 
-  // Replace the block below with the actual webGL code!
+  const url = "/tmpdata/Camera_01_4k.gltf/Camera_01_4k.gltf";
+  // const url = gltfFiles.gltf.url;
+  
+  let gltf;
+
+  try {
+    gltf = useGLTF(url)
+  } catch(err) {
+    // console.log(err)
+    // what horrible interface is this... why doesn't useGLTF just return a promise?!
+    err.then(() => {
+      // need to trigger a re-render so setting useless state
+      // TODO .. something else.. maybe fix three?!
+      setLoaded(true)
+    })
+  }
+
+  
+
+  // useEffect(() => {
+  //   // setGLTF(gltf);
+  //   console.log("gltf", gltf)
+
+  // }, [gltf]);
+
+
+  useEffect(() => {
+    // setGLTF(gltf);
+    console.log("gltf", gltf)
+  }, [loaded]);
+
+
+
+  // const gltf = useLoader(GLTFLoader, './Poimandres.gltf')
+
   return (
     <div className={styles.wrapper}>
-      <h1>WebGL goes here :)</h1>
-      <pre>{JSON.stringify(gltfFiles, null, 2)}</pre>
+      {/* because Canvas wraps in relative div with width/height 100%.. */}
+      <div style={{ position: "absolute", width: "100%", height: "100%" }}>
+        <Canvas>
+          <Suspense fallback={null}>
+            <ambientLight />
+            <perspectiveCamera fov={27} near={0.1} far={1000} position={[0, 0, 5]} />
+            <pointLight position={[10, 10, 10]} />
+            {gltf && gltf.scene && <primitive scale={10} object={gltf.scene} />}
+            <Environment preset="apartment" background={true} />
+            <OrbitControls
+              enablePan={true}
+              enableZoom={true}
+              enableRotate={true}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   )
 }
