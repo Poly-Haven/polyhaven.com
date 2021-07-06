cac84346018be2168de30c2c9bb8a652e48db737
Greg Zaal <gregzzmail@gmail.com>
2021-07-06 12:41:23 +0200

    Fix incorrect adsense implementation

--- components/Ads/GridTop.tsx
+++ components/Ads/GridTop.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react'
+
 import styles from './Ads.module.scss'
 
 const Ad = () => {
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  useEffect(() => {
+    if (isProduction) {
+      try {
+        // @ts-ignore - adsbygoogle not detected as a prop of window, since it's declared in _document.tsx
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }, []);
+
   // TODO hide for patrons
-  if (process.env.NODE_ENV !== "production") {
+  if (!isProduction) {
     return (
       <img src="https://via.placeholder.com/468x60" className={styles.placeholder} />
     )
@@ -14,7 +30,6 @@ const Ad = () => {
       style={{ display: "inline-block", width: "468px", height: "60px" }}
       data-ad-client="ca-pub-2284751191864068"
       data-ad-slot="9488083725"></ins>
-    <script dangerouslySetInnerHTML={{ __html: `(adsbygoogle = window.adsbygoogle || []).push({ });` }} />
   </>)
 }
 
