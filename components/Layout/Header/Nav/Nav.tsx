475fcbdce31344b96838a4741e26c0f5c18f4a23
Greg Zaal <gregzzmail@gmail.com>
2021-08-09 00:20:10 +0200

    Show login button in header.

--- components/Layout/Header/Nav/Nav.tsx
+++ components/Layout/Header/Nav/Nav.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useUser } from '@auth0/nextjs-auth0';
 
 import { MdMenu, MdExpandLess, MdAccountCircle } from 'react-icons/md'
+import { IoMdLogIn } from 'react-icons/io';
 
 import NavItem from './NavItem'
 
@@ -26,11 +27,13 @@ const Nav = () => {
         <NavItem text="News" link="https://www.patreon.com/polyhaven/posts?public=true" />
         <NavItem text="Support Us" link="https://www.patreon.com/polyhaven/overview" />
         <NavItem text="About/Contact" link="/about-contact" />
-        {!isLoading && user &&
-          <NavItem text={<MdAccountCircle />} link="/account">
+        <NavItem text={user ? <MdAccountCircle /> : <IoMdLogIn />} link="/account">
+          {user ?
             <NavItem text="Logout" link="/api/auth/logout" />
-          </NavItem>
-        }
+            :
+            <NavItem text="Login" link="/api/auth/login" />
+          }
+        </NavItem>
       </div>
       <div className={styles.menuToggle} onClick={toggle}>{navHide ? <MdMenu /> : <MdExpandLess />}</div>
     </>
