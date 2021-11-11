3a37f164ab1b608476f412d4362c3a0ec278c7a1
Greg Zaal <gregzzmail@gmail.com>
2021-11-11 12:51:35 +0200

    Add NewsCard component

---
+++ components/Library/Grid/GridItem/NewsCard.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+
+import Link from 'next/link';
+
+import { MdPause, MdPlayArrow } from "react-icons/md"
+
+import styles from './NewsCard.module.scss'
+
+const NewsCard = ({ key, topText, img, pausedImg, bottomText, link }) => {
+  const [pause, setPause] = useState(false)
+  const storageKey = `newsPause__${key}`
+
+  useEffect(() => {
+    const storedValue = JSON.parse(localStorage.getItem(storageKey))
+    const v = storedValue !== null ? storedValue : false
+    setPause(v)
+  }, []);
+
+  const togglePause = _ => {
+    const v = !pause
+    setPause(v)
+    localStorage.setItem(storageKey, JSON.stringify(v))
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.topText}>
+        <div className={styles.spacer} />
+        <img src='/Logo 256.png' />
+        {topText}
+        <div className={styles.spacer} />
+        {pause ?
+          <MdPlayArrow className={styles.pause} onClick={togglePause} />
+          :
+          <MdPause className={styles.pause} onClick={togglePause} />
+        }
+      </div>
+      <Link href={link}>
+        <a className={styles.img}><img src={pause ? pausedImg : img} /></a>
+      </Link>
+      <Link href={link}><a className={styles.bottomText}>
+        {bottomText}
+      </a></Link>
+    </div>
+  )
+}
+
+export default NewsCard
