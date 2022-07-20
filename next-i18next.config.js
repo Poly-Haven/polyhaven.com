d0cfd0085b3759e8a98aebab2eb9170ca7c2b574
Greg Zaal <gregzzmail@gmail.com>
2022-07-20 11:20:26 +0200

    Translate about page

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de'],
+    locales: ['en', 'de', 'zh'],
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development'
 };
