92c5dc0c2b9b7adfd35ddc31c99abc2a64e75152
Greg Zaal <gregzzmail@gmail.com>
2022-07-21 16:24:12 +0200

    i18 donate page

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -1,25 +1,39 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Link from 'next/link';
+import { useTranslation, Trans } from 'next-i18next';
 
+import LinkText from 'components/LinkText/LinkText';
 import TextPage from 'components/Layout/TextPage/TextPage'
 import Button from 'components/Button/Button';
 
 const Page = () => {
+  const { t } = useTranslation(['donate']);
+
   return (
     <TextPage
-      title="Once-off Donation"
-      description="How to make a once-off donation to Poly Haven."
+      title={t('donate:title')}
+      description={t('donate:description')}
       url="/donate"
     >
-      <h1>Make a once-off donation</h1>
-      <p>In order to be a sustainable open project, we like a <Link href="/finance-reports">predictable budget</Link>. Having a wildly varying income makes it difficult for us to plan ahead and hire staff long term. We would much prefer you to donate a small monthly amount over a long term than a single lump sum.</p>
-      <p>That said, we do of course accept donations of any kind :)</p>
-      <h2>Donations under $20</h2>
-      <p>Please join us temporarily on <a href="https://www.patreon.com/polyhaven/overview">Patreon</a>, wait until the end of the month when your payment is made, and then cancel your account.</p>
-      <h2>Larger donations</h2>
-      <p>To make a one-time donation, please use either PayPal or Ko-fi:</p>
-      <Button text="Donate on PayPal" href="https://paypal.me/polyhaven" />
-      <Button text="Donate on Ko-fi" href="https://ko-fi.com/polyhaven" />
+      <h1>{t('donate:title')}</h1>
+      <p><Trans
+        i18nKey="donate:p1"
+        t={t}
+        components={{
+          lnk1: <LinkText href="/finance-reports" />,
+          lnk2: <a href="https://www.patreon.com/polyhaven/overview" />,
+        }}
+      /></p>
+      <p>{t('donate:p2')}</p>
+      <h2>{t('donate:d1t')}</h2>
+      <p><Trans
+        i18nKey="donate:d1p"
+        t={t}
+        components={{ lnk: <a href="https://www.patreon.com/polyhaven/overview" />, }}
+      /></p>
+      <h2>{t('donate:d2t')}</h2>
+      <p>{t('donate:d2p')}</p>
+      <Button text={t('donate:d2b1')} href="https://paypal.me/polyhaven" />
+      <Button text={t('donate:d2b2')} href="https://ko-fi.com/polyhaven" />
     </TextPage>
   )
 }
@@ -27,7 +41,7 @@ const Page = () => {
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ['donate'])),
     },
   };
 }
