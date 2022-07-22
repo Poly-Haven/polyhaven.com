4a18bd4bda47214e473538ec5ef35fc728dde199
Greg Zaal <gregzzmail@gmail.com>
2022-07-22 17:25:10 +0200

    Formatting

--- pages/translate.tsx
+++ pages/translate.tsx
@@ -12,6 +12,8 @@ const Page = () => {
       url="/translate"
     >
       <h1>Help Translate Poly Haven</h1>
+      <p>Our assets are used by people all around the world - in fact more than half our users don't choose English as their first language!</p>
+      <p>If you'd like to help us translate the site into other languages, please reach out to us on <a href="https://discord.gg/Dms7Mrs">Discord</a> or by <Link href="/about-contact">email</Link>.</p>
       <div style={{
         display: 'flex',
         flexDirection: 'row',
@@ -26,10 +28,14 @@ const Page = () => {
           <LocaleFlag key={l} locale={l} flag={locales[l].flag} />
         )}
       </div>
-      <p>Our assets are used by people all around the world - in fact more than half our users don't choose English as their first language!</p>
-      <p>If you'd like to help us translate the site into other languages, please reach out to us on <a href="https://discord.gg/Dms7Mrs">Discord</a> or by <Link href="/about-contact">email</Link>.</p>
 
-      <h2>{"{{variables}}"}</h2>
+      <h2>Translation Guide:</h2>
+
+      <h3>Proper Nouns</h3>
+      <p>Make sure that the names of websites, software, and people are not translated, such as "Poly Haven", "Discord", "Patreon", etc.</p>
+
+      <hr />
+      <h3>{"{{variables}}"}</h3>
       <p>When you see a set of double brackets, like {"{{this}}"}, this indicates a variable that will be replaced by a value on the site.</p>
       <p>Make sure that sentence structure is maintained, but don't edit the text inside the brackets ("count" in this example).</p>
       <p>For example:</p>
@@ -39,7 +45,8 @@ const Page = () => {
       <q>John has 3 apples.</q>
       <q>约翰有 3 个苹果。</q>
 
-      <h2>{"<lnk>tags</lnk>"}</h2>
+      <hr />
+      <h3>{"<lnk>tags</lnk>"}</h3>
       <p>Similar to {"{{variables}}"}, there are often HTML tags of various kinds present in the source text.</p>
       <p>You can edit what's between the opening and closing tags, and fix sentence structure, but don't edit the tag name itself.</p>
       <p>{`The <strong> and <em> tags are generally used for emphasis, while <lnk> is a link to another web page.`}</p>
@@ -48,7 +55,8 @@ const Page = () => {
       <p>Would be translated to:</p>
       <q>{`感谢我们的<lnk>支持者</lnk>使 Poly Haven 成为可能`}</q>
 
-      <h2>Broken{"<lnk> formatting</lnk>"} and smileys: )</h2>
+      <hr />
+      <h3>Broken {"<lnk>formatting</lnk>"} and smileys :)</h3>
       <p>The database of translations is automatically filled with translations from Google Translate. This provides a good start, but it usually breaks formatting of tags and smileys.</p>
       <p>For example:</p>
       <q>This is a sentence with a {`<lnk>link</lnk>`}, and then a smiley :) Thanks!</q>
@@ -58,7 +66,8 @@ const Page = () => {
       <q>Esta es una oración con un <span className="text-green">{`<lnk>`}</span>enlace{`</lnk>`} y luego un emoticón <span className="text-green">:)</span> ¡Gracias!</q>
       <p>Notice there is no space before the {`<lnk>`} tag, and the colon is moved one character to the left.</p>
 
-      <h2>Maintaining Title Case</h2>
+      <hr />
+      <h3>Maintaining Title Case</h3>
       <p>Often the automatically translated text does not maintain the same case as the source text.</p>
       <p>For example:</p>
       <q>This Is A Title</q>
@@ -66,9 +75,6 @@ const Page = () => {
       <q>Dit is een titel</q>
       <p>When it should be:</p>
       <q>Dit Is Een Titel</q>
-
-      <h2>Proper Nouns</h2>
-      <p>Make sure that the names of websites, software, and people are not translated, such as "Poly Haven", "Discord", "Patreon", etc.</p>
 
     </TextPage>
   )
