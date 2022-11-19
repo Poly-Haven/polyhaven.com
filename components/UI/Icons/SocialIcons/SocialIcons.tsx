8e810130f69f5031fc6679141b03e36aad38e79b
Greg Zaal <gregzzmail@gmail.com>
2022-11-19 09:27:22 +0200

    Add mastodon link

--- components/UI/Icons/SocialIcons/SocialIcons.tsx
+++ components/UI/Icons/SocialIcons/SocialIcons.tsx
@@ -4,6 +4,7 @@ import { SiFacebook } from "react-icons/si";
 import { SiTwitter } from "react-icons/si";
 import { SiInstagram } from "react-icons/si";
 import { SiYoutube } from "react-icons/si";
+import { SiMastodon } from "react-icons/si";
 
 import styles from './SocialIcons.module.scss'
 
@@ -12,6 +13,7 @@ const SocialIcons = () => {
     <div className={styles.communityIcons}>
       <a href="https://discord.gg/Dms7Mrs"><SiDiscord /></a>
       <a href="https://www.patreon.com/polyhaven/overview"><SiPatreon /></a>
+      <a rel="me" href="https://masto.ai/@polyhaven"><SiMastodon /></a>
       <a href="https://www.facebook.com/polyhaven"><SiFacebook /></a>
       <a href="https://twitter.com/polyhaven"><SiTwitter /></a>
       <a href="https://www.instagram.com/polyhaven"><SiInstagram /></a>
