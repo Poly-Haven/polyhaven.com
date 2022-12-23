46954256c2374483532f167f13af6b07a24086f0
Greg Zaal <gregzzmail@gmail.com>
2022-12-23 09:19:18 +0200

    Use David Revoy's Cat Avatars instead of owls.

--- components/UI/Avatar/Patron.tsx
+++ components/UI/Avatar/Patron.tsx
@@ -1,18 +1,15 @@
 import { useTranslation } from 'next-i18next'
 import { timeago } from 'utils/dateUtils'
-import { stringHash } from 'utils/stringUtils'
 
 import styles from './Avatar.module.scss'
 
 const Avatar = ({ name, size, timestamp }) => {
-  const numImages = 333
-  const imgIndex = Math.abs(stringHash(name) % numImages)
-  const avatar = `https://cdn.polyhaven.com/site_images/owls/${imgIndex.toString().padStart(3, '0')}.png?width=${size}`
+  const avatar = `https://cats.polyhaven.org/api/cat?name=${name}`
 
   const { t } = useTranslation('time')
   return (
     <div className={styles.patron}>
-      <img src={avatar} width={size} height={size} className={styles.avatar} />
+      <img src={avatar} width={size} height={size} />
       <p>
         <strong>{name}</strong>
         <br />
