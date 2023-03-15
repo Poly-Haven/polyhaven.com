1be64a57e1eab6051b0cfb538da16764a056a2fc
Greg Zaal <gregzzmail@gmail.com>
2023-03-15 12:29:22 +0200

    Wording

--- components/Plugins/Blender.tsx
+++ components/Plugins/Blender.tsx
@@ -112,7 +112,8 @@ const Blender = ({ numAssets }) => {
         </p>
         <p>
           So far the sales of this add-on alone have allowed us to hire new artists and set up a studio to capture more
-          assets, work on R&D of entire new asset types, and travel around to world to capture content.
+          assets, work on R&D of entire new asset types, and travel around to world to capture free content for
+          everyone.
         </p>
         <p>
           Thanks for your support! <Heart color="#F96854" />
