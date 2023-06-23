542bd6d28b108c0fbb8bc9d892767be951d4930b
Greg Zaal <gregzzmail@gmail.com>
2023-06-23 10:08:50 +0200

    Fix tooltips

--- components/UI/Tooltip/Tooltip.tsx
+++ components/UI/Tooltip/Tooltip.tsx
@@ -1,20 +1,9 @@
-import ReactTooltip from 'react-tooltip'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
 
 import styles from './Tooltip.module.scss'
 
 const Tooltip = ({ id, place }) => {
-  return (
-    <ReactTooltip
-      id={id}
-      place={place}
-      delayShow={250}
-      multiline
-      border
-      borderColor="rgba(190, 111, 255, 0.5)"
-      backgroundColor="rgb(60, 60, 60)"
-      className={styles.tooltip}
-    />
-  )
+  return <ReactTooltip id={id} place={place} delayShow={250} className={styles.tooltip} />
 }
 
 Tooltip.defaultProps = {
