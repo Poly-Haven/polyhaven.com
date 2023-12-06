3293b3580e7de0abeff4c4e627577992ce28f08a
Greg Zaal <gregzzmail@gmail.com>
2023-12-06 15:05:42 +0200

    Hide authors with less than 2 assets by default, with button to show all

--- components/UI/Avatar/AllArtists.tsx
+++ components/UI/Avatar/AllArtists.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import dynamic from 'next/dynamic'
 
@@ -13,29 +14,41 @@ const AllArtists = ({ data }) => {
   authors.sort((a, b) => data[a].name.localeCompare(data[b].name))
   authors.sort((a, b) => data[b].assetCount - data[a].assetCount)
 
+  // Remove authors with less than {threshold} assets if not showing all
+  const [showAll, setShowAll] = useState(false)
+  const threshold = 2
+  if (!showAll) authors = authors.filter((author) => data[author].assetCount >= threshold)
+  const numFiltered = Object.keys(data).length - authors.length
+
   return (
     <div className={styles.allArtists}>
       {authors.map((author) => (
-        (<Link
+        <Link
           key={author}
           href={`/all?a=${author}`}
           prefetch={false}
           data-tip={`${author} (${data[author].assetCount} ${data[author].assetCount === 1 ? 'asset' : 'assets'})`}
           data-for="allAssets"
-          className={styles.avatar}>
-
+          className={styles.avatar}
+        >
           <Avatar id={author} size={50} />
           {data[author].regular_donor ? (
             <div className={styles.regularDonor} data-tip="Regular asset donor" data-for="allAssets">
               <Heart color="#F96854" />
             </div>
           ) : null}
-
-        </Link>)
+        </Link>
       ))}
+      {!showAll ? (
+        <p data-tip={`+${numFiltered} more authors with less than ${threshold} assets each`} data-for="allAssets">
+          <a style={{ margin: '0.5em', cursor: 'pointer' }} onClick={() => setShowAll(true)}>
+            +{numFiltered} more
+          </a>
+        </p>
+      ) : null}
       <Tooltip id="allAssets" />
     </div>
-  );
+  )
 }
 
 export default AllArtists
