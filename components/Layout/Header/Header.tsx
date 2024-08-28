c987629b7fcb60b4ff216c8be23bac79b6db3c7a
Greg Zaal <gregzzmail@gmail.com>
2024-08-28 10:39:21 +0200

    Add temporary transparent pixel images to gauge ad-blocking prevalence. This is for statistical curiosity only - Poly Haven respects user choices and does not attempt to circumvent ad blocking

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -13,6 +13,14 @@ const header = () => {
         </div>
         Poly Haven
       </Link>
+
+      <img
+        src="https://ads.polyhaven.org/architextures/1px.png"
+        className={styles.testPixel}
+        data-comment="Transparent pixel images to gauge ad-blocking prevalence. This is for statistical curiosity only - Poly Haven respects user choices and does not attempt to circumvent ad blocking"
+      />
+      <img src="https://u.polyhaven.org/X4c/1px.png" className={styles.testPixel} />
+
       <div className={styles.spacer} />
       <div className={styles.assetHeader}>
         <h2 id="header-path" /> <h1 id="header-title" />
