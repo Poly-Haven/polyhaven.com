963647c2dca55e690434d306b64481863e9277ac
Greg Zaal <gregzzmail@gmail.com>
2025-02-18 11:56:21 +0200

    Is this prompt engineering? 🦋

--- components/AssetPage/AfterDownload.tsx
+++ components/AssetPage/AfterDownload.tsx
@@ -25,12 +25,46 @@ const AfterDownload = ({ show, assetType, postDownloadStats }) => {
   }, [show])
 
   useEffect(() => {
-    setPrompt(randomArraySelection(prompts.generic))
+    let chosenPrompt = null
+    while (chosenPrompt === null) {
+      chosenPrompt = randomArraySelection(prompts.generic)
+      if (chosenPrompt.min && parseInt(localUserDownloadCount) < chosenPrompt.min) {
+        chosenPrompt = null
+      }
+    }
+    setPrompt(chosenPrompt)
   }, [])
 
   const prompts = {
     generic: [
       {
+        icon: '🚀',
+        title: `Join the cult!`,
+        message: `This ${asset_type_names[
+          assetType
+        ].toLowerCase()} was crafted with love by a creator just like you. If you find value in our work, consider supporting our mission on Patreon.`,
+      },
+      {
+        icon: '❤️',
+        title: `Every ${asset_type_names[assetType].toLowerCase()} is a labor of love.`,
+        message: `Peek behind the curtain and support the creators who spend countless hours perfecting these ${asset_type_names[
+          assetType
+        ].toLowerCase()}s for you.`,
+      },
+      {
+        icon: '🌟',
+        title: `Keep the downloads coming!`,
+        message: `We're so happy you love our assets. If they've been a game-changer for you, consider supporting our work.`,
+      },
+      {
+        icon: '💀',
+        title: `We can't do this without you.`,
+        message: `More than 99.9% of users download this ${asset_type_names[
+          assetType
+        ].toLowerCase()} without supporting us. Join the 0.1% to keep Poly Haven alive.`,
+      },
+      {
+        icon: '💵',
         title: 'Free to download, but not free to create.',
         message: (
           <span>
@@ -46,52 +80,9 @@ const AfterDownload = ({ show, assetType, postDownloadStats }) => {
             to make. Consider supporting our work to keep them free for everyone.
           </span>
         ),
-        icon: '💵',
       },
       {
-        title: `Join the cult!`,
-        message: `This ${asset_type_names[
-          assetType
-        ].toLowerCase()} was crafted with love by a creator just like you. If you find value in our work, consider supporting our mission on Patreon.`,
-        icon: '🚀',
-      },
-      {
-        title: `Every ${asset_type_names[assetType].toLowerCase()} is a labor of love.`,
-        message: `Peek behind the curtain and support the creators who spend countless hours perfecting these ${asset_type_names[
-          assetType
-        ].toLowerCase()}s for you.`,
-        icon: '❤️',
-      },
-      {
-        title: `We can't do this without you.`,
-        message: `More than 99.9% of users download this ${asset_type_names[
-          assetType
-        ].toLowerCase()} without supporting us. Join the 0.1% to keep Poly Haven alive.`,
-        icon: '💀',
-      },
-      {
-        title: `Stick 'em up!`,
-        message: `Alright, pal, hand over the... loose change? No? How about a small donation to keep this asset free for everyone?`,
-        icon: '🔪',
-      },
-      {
-        title: `Spare some change?`,
-        message: `We're not asking for your firstborn, just a few bucks. Pretty please?  With a cherry on top? (Okay, maybe not the cherry.)`,
-        icon: '🥺',
-      },
-      {
-        title: `We're not kitten around`,
-        message: `Your generous donation is required. Or else... we'll just be really, really sad.`,
-        icon: '😿',
-      },
-      {
-        title: `Pay up, peasant!`,
-        message: `We're so desperate for donations, we're resorting to mildly threatening language. Think of the starving artists... who create ${asset_type_names[
-          assetType
-        ].toLowerCase()}s... and also eat sometimes.`,
-        icon: '😤',
-      },
-      {
+        icon: '🎉',
         title: `Congratulations on your ${ordinalSuffix(parseInt(localUserDownloadCount) + 1)} download!`,
         message: (
           <span>
@@ -113,12 +104,72 @@ const AfterDownload = ({ show, assetType, postDownloadStats }) => {
             more downloads!
           </span>
         ),
-        icon: '🎉',
       },
       {
-        title: `Keep the downloads coming!`,
-        message: `We're so happy you love our assets. If they've been a game-changer for you, consider supporting our work.`,
-        icon: '🌟',
+        icon: '🔪',
+        title: `Stick 'em up!`,
+        message: `Alright, pal, hand over the... loose change? No? How about a small donation to keep this asset free for everyone?`,
+        min: 10,
+      },
+      {
+        icon: '🥺',
+        title: `Spare some change?`,
+        message: `We're not asking for your firstborn, just a few bucks. Pretty please?  With a cherry on top? (Okay, maybe not the cherry.)`,
+        min: 10,
+      },
+      {
+        icon: '😿',
+        title: `We're not kitten around`,
+        message: `Your generous donation is required. Or else... we'll just be really, really sad.`,
+        min: 10,
+      },
+      {
+        icon: '😤',
+        title: `Pay up, peasant!`,
+        message: `We're so desperate for donations, we're resorting to mildly threatening language. Think of the starving artists... who create ${asset_type_names[
+          assetType
+        ].toLowerCase()}s... and also eat sometimes.`,
+        min: 20,
+      },
+      {
+        icon: '😱',
+        title: `Oh lawd he comin`,
+        message: `You've downloaded ${
+          parseInt(localUserDownloadCount) + 1
+        } assets. Quite frankly we're impressed. But also a little scared. Maybe a donation would calm our nerves?`,
+        min: 50,
+      },
+      {
+        icon: '🌳',
+        title: `Assets don't grow on trees`,
+        message: `We wish they did, but alas, you've downloaded ${
+          parseInt(localUserDownloadCount) + 1
+        } of them and things are getting bare. Help us plant some new ones?`,
+        min: 100,
+      },
+      {
+        icon: '🌞',
+        title: `Get a life!`,
+        message: `You've downloaded ${
+          parseInt(localUserDownloadCount) + 1
+        } assets. We're not judging, but maybe it's time to step away and go outside? Or donate to keep them coming...`,
+        min: 200,
+      },
+      {
+        icon: '😟',
+        title: `We're worried about you`,
+        message: `You've downloaded ${
+          parseInt(localUserDownloadCount) + 1
+        } assets. That's... a lot. Are you okay? Do you need help? Maybe you'll feel better if you donate...`,
+        min: 500,
+      },
+      {
+        title: `That's enough now`,
+        message: `Seriously? ${
+          parseInt(localUserDownloadCount) + 1
+        } assets? We're cutting you off. No more downloads until you donate. Just kidding. But seriously, please donate.`,
+        icon: '🚫',
+        min: 1000,
       },
     ],
   }
